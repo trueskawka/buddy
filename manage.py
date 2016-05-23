6da04fab0516219cd70a495cf77eea1e87842bc7
Aneta <anetka.wlodarczyk@gmail.com>
2016-05-24 00:22:36 +0200

    Be helpful with how local_settings are created

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
+    if not os.path.exists('local_settings.py'):
+        raise Exception('missing local_settings.py, start with:\n    cp local_settings.py.dist local_settings.py')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "local_settings")
 
     from django.core.management import execute_from_command_line
